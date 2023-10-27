b7062a4aab7c06bf5cd6f5226188a786cd61b86e
My Name <mdarfinji45@gmail.com>
2023-10-27 15:57:39 +0800

    filtered functionality one

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { AiFillStar } from 'react-icons/ai';
 
 const Card = () => {
     return (
         <div>
-            
+                 <section className="card">
+          <img src="https://m.media-amazon.com/images/I/71yhoZP0l6L._AC_UY695_.jpg" alt="" className='card-img' srcset="" />
+          <div className="card-details">
+            <h3 className="card-title"> Shoe</h3>
+            <section className='card-reviews'>
+
+           <AiFillStar/> <AiFillStar/> <AiFillStar/>
+
+            </section>
+          </div>
+        </section>
+
+        <section className="card-price">
+
+            <div className="price">
+                <del>$300</del>200
+            </div>
+
+            <div className="bag">
+                
+            </div>
+
+        </section>
         </div>
     );
 };
--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,35 +1,14 @@
 import React from 'react';
 import { AiFillStar } from 'react-icons/ai';
 import './Products.css'
+import Card from '../Components/Card';
 
 const Products = () => {
     return (
 <>
     
     <section className="card-container">
-        <section className="card">
-          <img src="https://m.media-amazon.com/images/I/71yhoZP0l6L._AC_UY695_.jpg" alt="" className='card-img' srcset="" />
-          <div className="card-details">
-            <h3 className="card-title"> Shoe</h3>
-            <section className='card-reviews'>
-
-           <AiFillStar/> <AiFillStar/> <AiFillStar/>
-
-            </section>
-          </div>
-        </section>
-
-        <section className="card-price">
-
-            <div className="price">
-                <del>$300</del>200
-            </div>
-
-            <div className="bag">
-                
-            </div>
-
-        </section>
+        <Card></Card>
     </section>
 
 
