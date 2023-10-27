1e07e2f255238b6c1a65ac21e063439d60ebe60e
My Name <mdarfinji45@gmail.com>
2023-10-27 12:14:59 +0800

    navbar done

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Products = () => {
     return (
         <div>
-            
+            <h1>Proudcts</h1>
         </div>
     );
 };
--- src/Recommended/Recommended.js
+++ src/Recommended/Recommended.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Recommended = () => {
     return (
         <div>
-            
+            <h1>Recomemded</h1>
         </div>
     );
 };
